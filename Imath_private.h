ff5a07778a0c29b6a8c7af3bc128127159711d55
Dsmtyu <81510642+Dsmtyu@users.noreply.github.com>
2022-05-10 07:53:26 +0800

    Version 1.0.2.23
    New 新增
    1. powh 高精度幂运算
    2. aver 计算数组平均值
    3. fac 高精度阶乘运算
    修复已知问题
    1. add, sub, mul 数组越界

--- Imath_private.h
+++ Imath_private.h
@@ -5,19 +5,19 @@
 #define IMATH_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.36"
+#define VER_STRING	"1.0.0.37"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	36
+#define VER_BUILD	37
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.0.0.36"
+#define FILE_VERSION	"1.0.0.37"
 #define FILE_DESCRIPTION	"Imath, ÆæÆæ¹Ö¹ÖµÄÊýÑ§¿â(Developed using the Dev-C++ IDE)"
 #define INTERNAL_NAME	"Strang Math"
 #define LEGAL_COPYRIGHT	"GNU GENERAL PUBLIC LICENSE"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"Jmath"
 #define PRODUCT_NAME	"Imath"
-#define PRODUCT_VERSION	"1.0.0.36"
+#define PRODUCT_VERSION	"1.0.0.37"
 
 #endif /*IMATH_PRIVATE_H*/
